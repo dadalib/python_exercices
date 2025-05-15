0ab760677eaeb5c04bfd97f556aabeb47188e4cb
darwin <darwin.libioulle@gmail.com>
2025-05-15 22:03:19 +0200

    Add game correction

---
+++ casin_rusian_roulette_correction.py
@@ -0,0 +1,14 @@
+# The Rusian roulette gamei
+import os 
+from random import randrange
+from math import ceil
+
+# Declaration variable de depart
+money = 1000
+continue_to_play = True
+
+print("Do you want to play with : ", money," Euros")
+
+# Asking until player dont want to play
+while continue_to_play:
+    pass
