1baf56b9d81fbdcff5bd9058473f5ab4f973a8f6
darwin <darwin.libioulle@gmail.com>
2025-05-15 18:14:51 +0200

    Add fontions for casiono game

--- casino_rusian_roulette.py
+++ casino_rusian_roulette.py
@@ -1,4 +1,12 @@
 import random
+def user_budget():
+    try:
+        ask_budget = int(input("Give me your budget : "))
+    except ValueError:
+        print("You are enter a not interger number")
+
+    return ask_budget
+
 def user_input():
     try:
         ask_number = int(input("Give me your number  between 1 and 50 : "))
@@ -10,13 +18,33 @@ def user_input():
 def random_number():
     return  random.randrange(1,50)
 
-def check_if_user_win():
-    pass
+def half_budget(budget):
+    return int(budget/2) 
+
+def check_if_user_win(user_input,random_value,money_budget):
+    if user_input == random_value:
+        print("You win")
+    elif random_value%2==0 and user_input%2 ==0:
+        print("Pair : Here is hal of you money")
+        new_money_budget= half_budget(money_budget)
+        print("Here is your money " ,new_money_budget)
+    elif random_value%2!=0 and user_input%2!=0:
+        print("Not Pair : Here is hal of you money")
+        new_money_budget= half_budget(money_budget)
+        print("Here is your money ", new_money_budget)
+
+    else:
+    
+        print("You loose everything : ", int(money_budget-money_budget))
 
 
 if __name__=='__main__':
-    value=user_input()
-    random_number=int((random_number()))
-    print("random_number : ",random_number)
+    value_user=user_input()
+    print(value_user)
+    random_number=int(random_number())
+    print(random_number)
+    budget = user_budget()
+    check_if_user_win(value_user,random_number,budget)
+
     
 
