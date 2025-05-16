d9a69da6b504adbbe62bdf3d125eaaf8d91e4591
darwin <darwin.libioulle@gmail.com>
2025-05-16 19:05:10 +0200

    Add feature casino games

--- casin_rusian_roulette_correction.py
+++ casin_rusian_roulette_correction.py
@@ -11,4 +11,23 @@ print("Do you want to play with : ", money," Euros")
 
 # Asking until player dont want to play
 while continue_to_play:
-    pass
+    
+    # Player number to choose
+    number_play=-1
+    while number_play <0 or number_play > 49:
+        number_play = input("Play the number between 0 and 49"):
+        try:
+            number_play= int(number_play)
+        except ValueError:
+            print("You are not enter a number")
+            number_play = -1
+            continue
+        if number_play <0:
+            print("The number is negatif")
+        if number_play > 49:
+            print("Thenumber is higher than 49")
+
+        budget=0
+
+        while budget <= 0 or budget > money:
+            
