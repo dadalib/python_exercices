87911076a9f89d82e59b987fb45b9649ef55f762
User <darwin.libioulle@gmail.com>
2025-07-22 18:51:34 +0200

    add string ch

---
+++ string_characters.py
@@ -0,0 +1,20 @@
+STRING = " Dont shout at me"
+print(STRING.lower())
+
+# Empty string
+new_string = str()
+
+# while new_string.lower() != "q":
+#     print("Enter Q")
+#     new_string = input()
+
+# print("Thanks")
+
+new_string = "   test string manipulation"
+print(new_string.capitalize().strip())
+
+# use format
+name = "DL" 
+number = 3
+
+print("I am {0} the number {1}".format(name,number))
